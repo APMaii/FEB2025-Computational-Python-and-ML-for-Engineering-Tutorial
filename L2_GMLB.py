c7aa8118c77719470a8195154a3c288c66cea7e0
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-02-20 16:24:57 +0100

    Add files via upload

---
+++ L2_GMLB.py
@@ -0,0 +1,666 @@
+
+#salam emrooz jalaseye avale 
+#سلام امروز جلسه اوله
+#
+#
+#
+
+'''
+In The Name of GOD
+
+Ali Pilehvar meibody
+
+salam emrooz jalaseye 2 vome AI az gamlab hast
+
+
+
+bayad zabane python ro ejra konam
+
+
+--------
+zaban --> Vocab (kalamat) , grammar (dastoorat)
+
+VOCABS--> minevisi ->reserv shdoe
+
+yani python mishnasatesh --> print() , input(),len(),type()
+if , else , for , while ,..100
+
+reserved shode --> inaro msihnase
+
+
+
+----------Kole zaban python--------------
+RESERVED WORDS--> print()  input() if else for ,...... -->rangi msihan
+UNRESERVED WORDS---> sefid
+
+zarfan --> to mikhay yechizi tooshon zakhire koni
+
+
+zarf --> esm dare
+
+
+
+|      |
+|     | -----> esm dare
+|_____|
+
+dakhele zarfd yechizi hast k to zakhrie mikoni
+
+zarf--> moteghayer---> variable 
+ye esm dare --> yechizi ham dakhelesh --> to mikhay chiz save koni
+
+
+'''
+
+print()
+
+
+
+age=10
+
+sen=25
+
+
+sene_ali=40
+
+#va va va.....
+
+
+#-------esme zarf koli mogharart dare
+#esmo mitoni harchi bekhay bzari
+#farsi, englisi and....
+
+
+#-----1---> esmet nemitone toosh charactri joz _
+
+
+#zarfe sevom=
+
+#zarfe sevom=10
+
+#zarfe*sevom*=10
+
+zarfe_sevom=10
+
+
+
+#fasele, setare , character --> _
+
+
+#khode esm ba adad nabayad shoro she ama mitone adad tosh dahste bashe
+
+
+#2age=  --> ghalat
+age2=10
+
+zarf=100
+zarf2=200
+zarf3=300
+
+#2zarf= -->ghalat
+
+
+#case sensetive -->
+#a barabr nist ba A
+
+age=20
+Age=30
+
+#nokteye mohem --> esme zarfet nmitone kalamate
+#resrv shdo ebashe
+
+print()
+
+
+Print=10
+print2=10
+print_=10
+printt=10
+
+
+#--------------------------------------------
+'''
+harchizi k minevisi dakhele python
+ag hashtag bznim ya qout # comment dar nazar migire 
+va mipare
+
+baghie ro khat b khat ba deghat az bala b paeen
+az chap b rast mikhooone
+
+
+do no kalame darim ---->
+1---> reserved-->kari mikonan , mishnasateshon
+print, input, if , else, for --> rangeshon avaz mishe,
+
+
+2---> nemishnase-->unreserved-->sefid
+fght dar soorati mitoni estefade koni ke
+ina esme yek zarf (variable) bashe k dakheelsh
+chzii zakhire koni
+
+--------VARIABLES-----------
+yek zarf hastan yek esm daran , yek chizi ham dakheelsh mirizi (save mikoni)
+
+esme zarf --> unreserved--> englis A B C D E F ab s c
+
+esm
+sen
+age
+number
+adad
+ali
+
+ye ghanoni-->chaarcteri joz _ 
+fasele , * ^ $ 
+
+avale esmet adad bezani
+
+va ahamchnin az esme zakhrie shode ham nemitoni estefade koni
+
+
+#-----zarf  --> esm dar yechiz  mriizi dkahelesh
+esmesho fhmidm
+
+chi mirizam dakhelesh???
+
+
+1------> Numbers -----------------
+1.1. int (sahih) --> 1 10 1000 200
+1.2. float (ashari) --> 1.2 1.5 1.8
+1.3. complex --> 1j , 10+3j
+
+= , ** , * , / , + , - , %
+
+moghayese == , > , <
+
+
+2-------BOOLEAN
+True
+False
+
+
+
+3--------hoorofo , jomle, kalame zakhrie konam????
+zarf (variable) --> horof,joml,ekalame
+3------STR-----
+qoutation
+
+a b c d
+ali
+ali pilehvar meibody
+in the name of god i am the new .....
+
+ali $ asdjkdshakohsgasdkudhiah
+
+
+
+
+'''
+
+age=10
+
+weight=40.83
+
+vector=1j
+
+
+age2=20
+
+
+
+a=20
+b=30
+
+
+#----calculation operator-----
+
+# a ro be alaveye b konam zakhirash?
+#zakhire--> tooye c
+
+# a + b = c -->ghalat
+
+
+#aval zarfo besazi bad chizi troosh berizi
+
+
+
+c=a+b
+
+'''
+()
+** tavan
+* zarb
+/ taghsim
++ jam
+- tafrigh
+
+% baghimande
+
+= mosavi
+
+()
+
+'''
+a=10
+b=20
+
+#a+b = c
+
+c=a+b
+d=b-a
+e=a*b
+f=a/b
+g=a**b
+
+
+
+new=a**b*d-f
+
+#aval a **b mishe bad kolesh zarb dar d - f
+
+
+#ama man mikham aval d-f
+
+
+new=(a**b)*(d-f)
+
+
+#------------------------
+
+a=20
+b=30
+
+#a + b = c
+
+#a + b --> beriz too c
+
+#bayad aval zarf ro besazi -->mantego yadet bashe
+
+c=a+b
+
+
+
+
+a=20
+a=40
+
+
+
+
+a=20
+A=40
+
+
+#mohasebat ** * / + - %
+
+
+#--->moghayese???
+
+#aya a >b ?
+
+
+a>b
+
+a<b
+
+
+#dastoor 
+#to dastoor midi a ro khali kon b bezar
+
+a=10
+b=20
+
+b=a
+
+
+
+a=100
+
+
+a==b
+
+#fargeh = va ==
+
+
+
+a=True
+b=False
+
+
+
+a=10
+
+b=20
+
+#c=z
+#NameError: name 'z' is not defined
+
+
+z=10
+c=z
+
+
+#------
+a=10
+b=20
+
+#c=y
+#NameError: name 'y' is not defined
+y=100
+
+
+
+
+#---------------------------------------
+
+age=20
+sen=20
+
+#name=ali
+#NameError: name 'ali' is not defined
+
+#gara to nakhay esme zarf dar nazar begire
+#yani manzooret ine begii agah tooye yek zrf
+#bename name --> kalameye ali ro beriz na zarfe ali
+#bayad quotation --> ' '
+
+name='ali'
+
+#HOROF, kalame, jomle --> string -->str
+
+
+a='alipilehvarmeibody'
+
+b='sakuhsddoiyg9uydt3b8962tqbc9wegtrdb79q63tdrndrtqw68ng'
+
+a=10
+
+
+b='a'
+c=a
+
+#---------------------
+
+#reserve shode??
+
+print()
+#yek kalamss--> rangi shode-->yek kari mikone
+
+#--->print-->namayesh mide too console
+
+#tooye parantez benevsi
+
+
+print(40) #40
+
+print(328732987287297) #328732987287297
+
+print('salam')
+
+
+print('Hello World!') #Hello World!
+
+
+
+#koli zarf misakhti
+#Mitoni zarfato print koni
+
+
+a=40
+
+print(a)
+
+
+
+name='Ali Pilehvar Meibody'
+
+print(name) #Ali Pilehvar Meibody
+
+
+
+print('ali pilehvar meibody')
+
+#print(ali pilehvar meibody)
+
+
+a=20
+b=30
+
+print(a)
+print(b)
+
+
+#---- narenji-->tabe haye dakheli python built in function
+
+#print() -->namayesh
+
+#type() 
+
+
+type(a) #int
+
+name='ali'
+
+type(name) #str
+
+
+#len() --> andaze ro mige
+
+name='alipilehvarmeibody'
+len(name) # 18
+
+name='ali'
+len(name)  #3
+
+name='ali '
+len(name) # 4
+
+
+name='ali pilehvar meibody'
+len(name) #20
+
+
+#-------------------PRINT()------------
+
+#chanta chiziro print konam chi????
+a=20
+b=30
+c=40
+
+
+print(a)
+print(b)
+print(c)
+'''
+20
+30
+40
+'''
+
+
+#20 30 40
+
+#comma , 
+
+
+print(a,b,c) #20 30 40
+
+name='ali pilehvar'
+
+age=40
+
+print(name,age)
+
+#print(name age)
+
+
+print(name,30)
+
+
+print('ali',age)
+
+
+print('ali','age')
+
+
+#------------------------------------------------
+
+len(name) #12
+
+c=len(name)
+
+print(c)
+#12
+
+
+#tabe haro too dar to estefade koni
+
+
+print(len(name)) #12
+
+
+
+#-------------------------------------------
+#=========================================
+#=========================================
+
+#=========================================
+#=========================================
+
+#yek zarf daram esm dare --> name
+#dakhelesh mohtaviat dare --> alipilehjvar (value)
+#type --> str --> string reshte (charcter,horof,kalame, jomle)
+#size--len --> 11
+    
+    
+name='alipilehvar'
+#--> agha samte rasto negah
+
+print(name) #alipilehvar
+
+type(name) # str
+print(type(name)) #<class 'str'>
+
+
+len(name) # 11
+print(len(name)) #11
+
+b=10
+c=30.5
+
+print(name,b+c)
+#alipilehvar 40.5
+
+
+print(len(name)*b+c)
+#140.5
+
+
+
+name='alipilehvar'
+
+#string---> str --> string-->reshtee
+#reshte ee az horoooof
+
+#mikham zkahriash konm to yek zrfi bename new
+
+new='p'
+
+new=name
+
+
+
+
+#-----------------------------------
+'''
+ai.2024.pilehvar@gmail.com
+
+
+subject --->
+
+L2_name_lname
+
+L2_Amir_Amiri
+
+L3_AMir_Amiri
+
+
+L4_AMIR_AMIRI
+
+....
+
+
+
+
+TASK1----->
+
+esmeto too yek zarff beriz
+reshtato too yek zarf beriz
+
+ba estefade az yek print, va dota zarf bayad jomleye zir print she
+
+
+salam man (esmet) hastam va b reshjteye (felanm) alaghe daram
+
+
+
+TASK2-->
+esmeto bedone fasele benevis beriz too zarf
+
+andazasho be tavane 2 kon -10 print she
+
+
+
+TASK3--->
+3 ta adad ro zakhrie kon
+
+yek moadeley bozorg benevis (tavan , taghsim , + ) 
+ham,rah ba parantez
+
+
+
+
+Ai.2024.pilehvar@gmail.com
+
+
+subject
+
+L2_Amir_Amiri
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
