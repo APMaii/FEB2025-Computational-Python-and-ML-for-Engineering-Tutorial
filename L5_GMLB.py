0f66de13a346a6c95d9fc31f2244ba09d6ba18b7
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-03-15 02:53:21 +0330

    Add files via upload

---
+++ L5_GMLB.py
@@ -0,0 +1,1242 @@
+"""
+In The Name of GOD
+
+
+@author: Ali Pilehvar Meibody
+
+
+
+L5_GMLB
+
+
+"""
+
+print('salam')
+
+
+abs(-10)
+
+max(10,20)
+min(10,20)
+
+2**3 #8
+
+pow(2,3) #8
+
+
+#---------
+#print() len() type() inptu() open()
+#----------
+
+'''
+reserved --> keywords---> IF
+
+
+
+
+'''
+
+
+
+print('salam')
+
+
+'''
+**shart--> javabesh true false , > < == !=
+.str function
+
+
+
+if shart:
+    dastoooor
+    
+    
+agar hsart True -->dastor anbjam mishe
+
+
+
+'''
+
+
+a=20
+
+
+if a>10:
+    print('salam')
+
+
+
+#----------
+#if else---->
+#do rahi baz mikone
+
+
+if a>20:
+    print('kare 1')
+else:
+    print('kare 2')
+    
+    
+'''
+
+if shart:
+    dastoor1
+    dastoor2
+    dastoor2
+
+else:
+    dastoor4
+    dastoor5
+    dastoor6
+    
+    
+mamoli done done
+harkodi bzni
+
+
+if else
+
+
+'''
+    
+    
+'''
+///////////JUST IF/////////////
+
+         --->if --->(ezafe)
+main ---------------------------> edameye main
+      
+
+
+///////////IF ELSE/////////////
+
+
+         --->if --->kar1
+main -----             -------> edameye main
+          --->else --->kar2
+
+
+///////////IF ELIF/////////////
+else if ---> elif
+
+
+
+         --->if --->kar1
+main -----             -------> edameye main
+                       ---->
+          --->else ---> if else
+    
+
+          
+          
+
+'''
+
+
+
+if a>20:
+    print('kare 1')
+else:
+    print('kare 2')
+    
+    
+#na agha ag bishtar az 20  shod kare 1
+#age nashod (nemigi kare 2) --.dorahi baz mikoni
+
+
+if a>20:
+    print('kare 1')
+
+#in tiek baraye afradi hast k a<20    
+elif a>15:
+    print('kare2')
+
+else:
+    print('kar3')
+    
+
+
+
+#------------------------
+'''
+
+if shart:
+    dastoor1
+    dastoor2
+    dastoor3
+    
+else:
+    dastoor1
+    ......
+    
+    
+    
+age man chanta shart bekham chi??
+
+
+
+<<<<ag bsihtar az 1 shart kahstam chi?>>>
+2 halat bbin vaghty to 2 ta shart dri
+2 ta shart a>20 , b>30
+
+dota chiz migi be farsz
+
+
+
+1--> ham sharte 1 ham sharte 2 bayad true she , joftesh
+and --> (ham, joft, hame , va)
+
+
+
+2---> ya sharte1 ya sharte2 --> hade aghal yekishon
+or --> (ya , hadeaghal yeki , )
+
+
+
+
+
+if shart:
+    dastoooor
+
+
+
+age shart1 va sharte 2 anjam shdo dastoor ro anjam, bde
+(va-->joftesh)
+
+if shart1 and shart2:
+    dastoooor
+    
+    
+    
+    
+#ya
+
+if shart1 or shart2:
+    dastoooor
+
+
+'''
+
+
+
+
+sen=20
+
+if sen>=18:
+    print('salam')
+    
+#yek shart yek kar
+
+
+
+
+sen=20
+ghad=190
+
+if sen>18:
+    print('salam')
+    
+
+
+#dota shart 
+# yedone shart sen>18 , ghad>180
+
+
+'''
+
+har joftesh bayad sedgh kone
+
+ham , joft, hamashonn
+
+and
+'''
+
+
+
+if sen>18 and ghad>180:
+    print('salam')
+    print('khobi?')
+    #
+    #
+    #
+    #
+    #
+
+
+'''
+
+agha hadeghal 1 kodomesh 
+ya ghad bala 180 , ya sen bala 18
+
+'''
+
+
+if sen>18 or ghad>180:
+    print('salam')
+    
+    
+
+#-----------
+
+print(True)
+print(False)
+print(True,False)
+
+
+print(True and True) #True
+print(True or True) #True
+print(False and False) #False
+print(False or False) #False
+
+
+
+print(True and False) #--->False
+
+print(True or False) #-->True
+
+
+shart1=True
+shart2=False
+
+#and , or --> Tru False
+
+if shart1 and shart2:
+    print('salam')
+
+
+if shart1 or shart2:
+    print('salam')
+    
+    
+#------------------------
+#------------------------
+'''
+
+
+daneshgah,
+amazon
+digikala
+
+
+dasr entkehab
+codesho mzini 
+
+
+behet naamyesh mide mige agha taeed mikoni?
+
+
+digikala , divar
+
+code mahsool
+esme mahsoool
+gheyamt midi
+
+
+namayesh mdie sabt va sabt mikone
+
+
+
+'''
+#-----Product management---------
+'''
+
+kilidvazhe---->
+
+Farsi-->python
+
+
+begire aszs user--->input()
+namayesh bede -->print()
+
+age , shart ----> if , if else , ....
+
+
+'''
+
+'''
+
+
+az user code, name , price yek product ro begire
+behesh b formate zir neshoon bde
+
+
+
+
+
+'''
+
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+
+print('name mahsoole shoma hast:',name)
+print('code mahsoole shoma hast:',code)
+print('gheymate mahsoole shoma hast:',price)
+#print('aya taeed mikonid? (yes/no)')
+answer=input('aya taeed mikonid? (yes/no)')
+
+
+
+a=40
+print('salam man ali hastam va man a salame')
+
+#f string
+
+
+print(f'salam man ali hastam va man {a} salame')
+
+
+
+
+#--------------------
+
+
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+print(text)
+
+
+
+
+
+#------------------------------
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+
+print(text)
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+
+
+'''
+
+#-->soale 1 (case1)
+user agar neevsht yes --> system benevise sabt shod
+
+#--->soale2 (case2)
+user agar neevsht yes --> system benevise sabt shod
+agar nevesht NO --> system benevise sabt nashod
+
+
+
+'''
+
+
+
+
+#------------------------------
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+print(text)
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+#----> shart --> check kone 
+#
+#answer --> yes
+#answer=='yes'
+#a==b
+
+
+if answer=='yes':
+    print('sabt shod')
+
+
+#--------------
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+print(text)
+
+
+#----> shart --> check kone 
+#
+#answer --> yes
+#answer=='yes'
+#a==b
+
+#harchize dg ee nevesht --> bege motasefgane sabt nashod
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+
+if answer=='yes':
+    print('sabt shod')
+else:
+    print('sabt nashod motasefane')
+    
+#yes ---> sabt shod
+#no--->sabt nashod
+#djuhygdsyg ---> sabt nashod
+
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+
+if answer=='yes':
+    print('sabt shod')
+
+if answer=='no':
+    print('sabt nashod')
+    
+    
+
+    
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+
+if answer=='yes':
+    print('sabt shod')
+
+elif answer=='no':
+    print('motasefane sabt nashod')
+    
+else:
+    print('ba yes ya no javab dahid')
+      
+    
+    
+    
+    
+    
+    
+#-----------------------------
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+print(text)
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+if answer=='yes':
+    print('sabt shod')
+
+answer1=' yes'
+answer2='yes'
+
+print(answer1==answer2) #False
+
+print(len(answer1)) #4 space y e s
+print(len(answer2)) #3  y e s
+
+answer1[0] #' '
+answer2[0] #'y'
+
+
+
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+if answer=='yes':
+    print('sabt shod')
+    
+    
+    
+    
+
+
+#space zad
+
+# yes   yes yeksan dar nazar bgire?
+
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+if answer=='yes' and answer==' yes':
+    print('sabt shod')
+# ham yes ham,  yes
+
+if answer=='yes' or answer==' yes':
+    print('sabt shod')
+
+
+
+#  yes
+#   yes
+#yes 
+#yes  
+
+#Yes
+#yEs
+#yeS
+#YES
+
+#answer --> type? -->string ---> str function
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+
+new_answer=answer.strip()
+
+
+name1=' ali'
+name2='ali '
+name3=' ali '
+    
+#space chap , raste yek kalame ro 
+name1.strip() #'ali'
+name2.strip() #'ali'
+name3.strip() #'ali'
+
+name4=' ali pilehvar '
+name4.strip() #'ali pilehvar'
+
+
+answer=' yes '
+answer.strip() #'yes'
+
+
+
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+new_answer=answer.strip()
+if new_answer=='yes':
+    print('sabt shod')
+
+
+#yes yES?
+#lower --> 
+#Yes
+#yeS
+#YES
+
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+new_answer=answer.strip()
+new_answer2=new_answer.lower()
+
+if new_answer2=='yes':
+    print('sabt shod')
+
+
+
+#yes --> YES
+#yeS --> YES
+
+#---> YES
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+new_answer=answer.strip()
+new_answer2=new_answer.upper()
+
+if new_answer2=='YES':
+    print('sabt shod')
+
+
+
+
+#-----Professional
+'''
+darse kamel
+
+kilid vazhe ---> begire (input) , neshon bede (print)
+age , ama agar, shart ,check --> if 
+
+function baraye taghir
+
+
+
+zarf zarf bnvsi
+badan professional -_.zarfaro edgham kon
+
+
+'''
+
+answer=input('aya informatione balaro taeed mikoni? (yes/no) :')
+if answer.strip().lower()=='yes':
+    print('sabt shod')
+
+
+
+#----nakhaan mikon codeto
+#if input('aya informatione balaro taeed mikoni? (yes/no) :').strip().lower()=='yes':
+#    print('sabt shod')
+
+
+
+#if answer.strip().lower()=='yes':
+#answer.lower().strip()
+    
+
+
+
+
+#-----------------------
+#-----> nemooneye A**
+
+name=input('salam name mahsooleton chist? :')
+code=input('code mahsooletoon chist?:')
+price=input('gheymate mashooleton chand hast?:')
+
+
+text=f'''
+
+----------PLUTUS MANAGEMENT-------------
+
+Product Information:
+    
+    Product Name : {name}
+    Product Code : {code}
+        
+        
+        
+        Total Price: {price}
+            
+            
+Plutus Co. 2024
+
+'''
+print(text)
+
+#-----> nemooneye A**
+
+
+
+
+a=input('usere 1 lotfan adad begoo')
+
+print('------------------')
+print('------------------')
+
+print('------------------')
+
+print('------------------')
+
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+print('------------------')
+
+
+b=input('usere 2 lotfan hads bezan')
+
+
+
+
+#-----------------------------
+#-----------------------------
+#----------IF-----------------------
+
+
+#---------------man bekham yek kari bokonam 
+#yek chizi ro tekrar konm chi??
+
+#keyword--->code ro beham mirizn
+
+#
+
+print('salam')
+
+
+
+
+#print('salam')
+#20 bar type konm
+
+
+
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+print('salam')
+
+
+#keywords------>
+#-->1 --> sharti (if , elif , else)
+
+
+#-->2-->halghe ( tekrar, halghe o ....)
+#yek kario --> shoma niaz b halghe dariiii
+#Halghe ha
+
+
+print('salam')
+
+
+
+
+#-------
+#
+'''
+
+
+mofasere python -->
+niaz be yechizi dar ebename shomarande
+
+yechizi bayad dashte bashe
+
+y zarfe --> shomarande
+
+ejr amikone 
+
+sh --> shoamrande
+
+
+sh=0
+
+sh=1
+
+
+sh=2
+
+sh=3
+sh=4
+sh=5
+
+sh=10
+
+
+
+
+i 
+shomarande
+
+
+'''
+
+#code-->harchi
+#print()
+
+print('salam')
+
+
+
+#for --> halgeh bezan
+
+
+
+for i in range(0,10):
+    print('salam')
+
+
+
+'''
+
+
+
+for i in range(0,10):
+    dastoooor
+    
+    
+    
+    
+range(0,10) --> 0 1 2 3 4 5 6 7 8 9 
+
+
+i ---> 0 , 1 2 3 4 5 6 7 8 9
+
+
+hey i-->0 dastoor1
+i-->1 dastooor
+i-->2 dastoor
+....
+i-->9 dastooor 
+
+
+
+boro biron az halghe
+
+
+
+
+
+'''
+
+print('salam')
+
+
+
+
+
+
+for i in range(0,10):
+    print('salam')
+
+
+
+'''
+ADI-->
+salam
+
+
+in paen adi ejra nmikone
+
+
+BA FOR-->
+i-->0 , salam
+i-->1 salam
+i-->2  salam
+i--.3  salam
+i-->4 salam
+i-->5 salam
+i-->6 salam
+i-->7 salam
+i-->8 salam
+i-->9 salam
+
+
+
+edame mdie b edamey ecode (main)
+
+
+
+
+
+
+'''
+
+
+
+
+for i in range(0,10):
+    print('salam')
+
+
+
+for k in range(0,10):
+    print('salam')
+
+#10 ta salam 
+#k--.1 k--.2 k-->3
+
+for alipilehvar in range(0,10):
+    print('salam')
+
+#alipilehvar --.1 , 2,3,45,6,7
+
+
+
+
+for i in range(0,10):
+    print('salam')
+
+
+'''
+
+1*******
+for --> halghe
+
+2*****
+shomarande dari (oon posshte)
+
+in range -->  tedade shomarande moheme
+
+
+3***
+hardafe shomarande ro taghir mdie
+va dastooor ro ejra mikone
+
+
+'''
+
+
+for i in range(0,10):
+    a=10
+    a=a+1
+
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+a=10
+a=a+1
+
+
+
+
+
+for i in range(0,10):
+    print('salam')
+
+
+
+
+
+for i in range(0,10):
+    print(i)
+
+
+'''
+range(start,end+1)
+
+start ,.. .,...., end
+
+range(0,10)
+
+0 1 2 3 4 5 6 7 8 9
+
+
+
+
+i-->0 / dastoor print(i) --> print(0) --->0
+i-->1  / dastooor print(i) --> print(1) -->1
+i-->2 / dastoor print(i) --->print(2)-->2
+i-->3 / dastoor print(i) --->print(3)-->3
+i-->4/ dastoor print(i) --->print(4)-->4
+i-->5/ dastoor print(i) --->print(5)-->5
+i-->6 / dastoor print(i) --->print(6)-->6
+i-->7/ dastoor print(i) --->print(7)-->2
+i-->8 / dastoor print(i) --->print(8)-->8
+i-->9 / dastoor print(i) --->print(9)-->9
+
+i-->10?
+
+
+
+'''
+
+
+for i in range(0,10):
+    print(i)
+    
+'''
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+'''
+
+
+ 
+    
+    
+
+
+
+'''
+(**)
+TASK1
+machine hesabi benevisid k az user
+do adad begire va badesh alamat (+ , - ,...)
+badesh --> print kone javab ro
+
+
+
+
+TASK2
+machine hesabi benevisid k az user
+do adad begire va badesh alamat (jam,tafrigh,zarb,menha)
+badesh --> print kone javab ro
+
+
+
+(**)
+TASK3 (interemediate) 
+nemooneye A** vaghty price migire 
+bayad 30 darsad takhfif rooye adad bezane
+bad namayeshesh bede
+
+
+
+TASK4 (bad Task3) -->hard
+nemooneye A** vaghty price migire 
+bayad 30 darsad takhfif rooye adad bezane
+bad namayeshesh bede
+
+**agar fard beja price (adad), horof nevesht
+#code nabayd error bede bayad benevise
+#lotfan adad vared konid
+
+
+
+TASK5
+bazi ba doost
+# 2 ta user darim 
+
+#yeki ye adad minevise (user1) 0 ta 100
+**ag bishtar az 100 adad dad bege na nemishe dobare begire
+
+#print('-----------------')
+
+#user 2 hey 5 ta forsat dare 
+#too 5 bar fosat bayad hads bezane
+#ag doros hads zad benevise tabrik migam
+
+
+
+TASK6
+hamoon task5 --> bayad machien rahnamee ham kone
+yani bege bishtar begoo
+kamtaresh kon
+
+
+#-----------------
+
+(**)
+TASK7
+7.1.biad az 0 ta 200 ro print kone
+7.2. biad 20 bar salam ro print kone
+7.3. 20 bar salam ro print kone va adad 
+7.4 in shekle zir ro print kone
+salam user  1  khosh amadid
+salam usere 2 khosh amadid
+100 user --> khsoh amad gooe kone
+
+
+
+TASK8
+donbaleye fibonachi ro benevise (print kone) ta 
+100
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
